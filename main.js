c9ce9703175c107fae7381ee907d8069fd83db66
marklyhalley <hall.secure@proton.me>
2025-09-20 16:48:45 -0300

    resolvendo erro letra+numero

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 function calcular() {
-    let horaI = parseInt(document.getElementById("horaIni").value)
-    let minI = parseInt(document.getElementById("minIni").value)
-    let horaF = parseInt(document.getElementById("horaFim").value)
-    let minF = parseInt(document.getElementById("minFim").value)
+    let horaI = (document.getElementById("horaIni").value)
+    let minI = (document.getElementById("minIni").value)
+    let horaF = (document.getElementById("horaFim").value)
+    let minF = (document.getElementById("minFim").value)
     function validarnum(hora,min){
         if (isNaN(hora) ||isNaN(min) ||hora<0 || hora>23 || min<0 || min>59) return false
         return true;
     }
+
     if (!validarnum(horaI,minI)|| !validarnum (horaF,minF)){
-    window.location.reload()
-    alert ('Erro: Favor inserir horários válidos') 
+    alert ('Erro: Favor inserir horários válidos')
+    return
     }
+    horaI = parseInt(horaI)
+    minI = parseInt(minI)
+    horaF = parseInt(horaF)
+    minF = parseInt(minF)
     let allminI = (horaI*60)+minI
     let allminF = (horaF*60)+minF
     if (allminI>allminF) {
